c3be2e44ca35104f47ed4090ae4eb682f4f9528d
Tyler Baker <tyler.baker@linaro.org>
2015-12-07 21:05:29 -0800

    ubuntu: display-capture: no host key check for all

--- ubuntu/scripts/capture-photo.sh
+++ ubuntu/scripts/capture-photo.sh
@@ -1,4 +1,4 @@
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $1 -C "cd /tmp && mkdir dslr && cd dslr && gphoto2 --capture-image-and-download"
-scp $1:/tmp/dslr/* .
-ssh $1 -C "rm -rf /tmp/dslr"
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $1:/tmp/dslr/* .
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $1 -C "rm -rf /tmp/dslr"
 lava-test-run-attach capt0000.jpg application/x-jpg
