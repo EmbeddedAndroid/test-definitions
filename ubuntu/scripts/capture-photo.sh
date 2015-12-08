99ebff06d07f2f3c4e686383e3a0d122de8b0032
Tyler Baker <tyler.baker@linaro.org>
2015-12-07 20:55:09 -0800

    ubuntu: display-capture: fix typo

--- ubuntu/scripts/capture-photo.sh
+++ ubuntu/scripts/capture-photo.sh
@@ -1,4 +1,4 @@
-sh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $1 -C "cd /tmp && mkdir dslr && cd dslr && gphoto2 --capture-image-and-download"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $1 -C "cd /tmp && mkdir dslr && cd dslr && gphoto2 --capture-image-and-download"
 scp $1:/tmp/dslr/* .
 ssh $1 -C "rm -rf /tmp/dslr"
 lava-test-run-attach capt0000.jpg application/x-jpg
