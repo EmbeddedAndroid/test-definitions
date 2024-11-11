1eac658547266f170d518480a0ba12eb777c5119
anilyada <anilyada@qti.qualcomm.com>
2024-11-11 15:42:58 +0530

    added arch timer testcase in automated/linux/arch-timer folder

---
+++ automated/linux/arch-timer/arch-timer.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+# shellcheck disable=SC1091
+
+HOST_OUTPUT="$(pwd)/output"
+RESULT_FILE="${HOST_OUTPUT}/result.txt"
+export RESULT_FILE
+
+
+# shellcheck disable=SC1091
+. ../../lib/sh-test-lib
+# shellcheck disable=SC1091
+. ../../lib/android-test-lib
+
+sudo mkdir -p "${HOST_OUTPUT}"
+
+adb_push "./device-script-arch-timer.sh" "/var/persist"
+
+info_msg "About to run arch timer test on device"
+adb shell "/var/persist/device-script-arch-timer.sh" | sudo tee "${RESULT_FILE}"
---
+++ automated/linux/arch-timer/device-script-arch-timer.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Function to get the numerical values from /proc/interrupts
+get_values() {
+    cat /proc/interrupts | grep arch_timer | awk '{print $2, $3, $4, $5, $6, $7, $8, $9}'
+}
+
+# Get initial values
+initial_values=($(get_values))
+
+# Wait for 10 seconds
+sleep 10
+
+# Get new values
+new_values=($(get_values))
+
+# Check if all new values are greater than initial values
+pass=true
+for i in ${!initial_values[@]}; do
+    if [ ${new_values[$i]} -le ${initial_values[$i]} ]; then
+        pass=false
+        break
+    fi
+done
+
+# Print result
+if $pass; then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
